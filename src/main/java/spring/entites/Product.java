d03dab55dc6cedb8d86cdbba1cce30bd54468727
Yuriy <yurgennik@gmail.com>
2021-08-30 17:24:23 +0500

    Lesson_3 Spring MVC

---
+++ src/main/java/spring/entites/Product.java
@@ -0,0 +1,33 @@
+package spring.entites;
+
+public class Product {
+    private int id;
+    private String title;
+    private int cost;
+//    private String[] programmingLanguages;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/newProduct.jsp
@@ -0,0 +1,16 @@
+
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add new product</title>
+</head>
+<body>
+<h4>Add new product</h4>
+<form:form action="addProduct" modelAttribute="product">
+    <p> Title <form:input path="title" type="text" /></p>
+    <p> Cost <form:input path="cost" type="text" value="0.00"/></p>
+    <p> <input type="submit" value="Add"/></p>
+</form:form>
+</body>
+</html>
